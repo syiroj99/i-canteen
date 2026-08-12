6dad2f7f1ce36f6439a47fea6d2ff3f20c27f1be
syiroj99 <rizkysyiroj99@gmail.com>
2026-08-12 15:04:32 +0700

    Create supabase-client.js

---
+++ js/supabase-client.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://ilgbwqwpcjturkryqbbf.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_86fYlNzcV1JP8NdtbUOokw_u3gZAQBw';
+
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
